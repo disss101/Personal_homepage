5e96b74411c37aaaf535469b72447c866f1e6411
disss101 <2470464202@qq.com>
2026-07-05 14:17:56 +0800

    Configure Supabase client

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.GIGOTON_SUPABASE = {
-  url: "",
-  anonKey: ""
+  url: "https://ppakwgooodteowtybdvs.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwYWt3Z29vb2R0ZW93dHliZHZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyMjUyMTQsImV4cCI6MjA5ODgwMTIxNH0.XzGXzHRcFwTquKn2SRCiBM58e3TCHLAQ006AiDWyA3A"
 };
